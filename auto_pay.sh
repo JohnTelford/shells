e4fd12ff309347e0503a0c1fe688a35428177868
John <john@johntelford.com>
2023-04-15 17:55:16 -0700

    refactor
    Signed-off-by: John <john@johntelford.com>

--- auto_pay.sh
+++ auto_pay.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #	auto_pay.sh
 
-pdfgrep -i -P "netflix|electric|gas|adobe|farm|github|att\*|comcast|elect|natural" *2-18\ 2023.pdf | awk '{print $3, $NF}'
+pdfgrep -i -P "netflix|electric|gas|adobe|farm|github|att\*|comcast|elect|natural" *2-18\ 2023.pdf | awk '{ print $3, $NF }'
